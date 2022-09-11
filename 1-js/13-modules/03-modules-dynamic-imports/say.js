c456f1ad30b196ba3b37641e2625017725fbb8c0
luyang93 <luyangshare93@gmail.com>
2022-09-12 02:23:19 +0800

    03-modules-dynamic-imports.

---
+++ 1-js/13-modules/03-modules-dynamic-imports/say.js
@@ -0,0 +1,12 @@
+// 📁 say.js
+export function hi() {
+  console.log(`Hello`);
+}
+
+export function bye() {
+  console.log(`Bye`);
+}
+
+export default function() {
+  console.log('Module loaded (export default)!');
+}
